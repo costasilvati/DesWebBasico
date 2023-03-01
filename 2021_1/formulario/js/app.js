4d92b4356d64e10d14f9cacd775866a446da15b4
Costa-Silva <julianacostasilvati@gmail.com>
2023-02-28 21:46:25 -0300

    Código de Aula 1

--- 2021_1/formulario/js/app.js
+++
@@ -1,9 +0,0 @@
-import {valida} from './master.js'
-
-const inputs = document.querySelectorAll('input')
-
-inputs.forEach(input =>{
-    input.addEventListener('blur',(evento) =>{
-        valida(evento.target)
-    })
-})
