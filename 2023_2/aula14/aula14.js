760bae8c905818c4cb240997aa399d31c7111f03
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2023-11-10 19:01:17 -0300

    código de aula

--- 2023_2/aula14/aula14.js
+++ 2023_2/aula14/aula14.js
@@ -1,3 +1,46 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const formulario = document.getElementById('formulario');// id do form
+   
+    formulario.addEventListener("submit", function(event){
+        event.preventDefault();
+        // sua lógica
+        let numero = document.getElementById('tabuada').value;
+        let divResult = document.getElementById('result');
+        divResult.innerHTML = "";
+        for(i=1; i<=10; i++){
+            let result = parseInt(numero) * i;
+            let texto = document.createTextNode(`${numero} x ${i} = ${result}`);
+            let node = document.createElement('p');
+            node.appendChild(texto);
+            divResult.appendChild(node);
+        }
+        // sua lógica
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function imprimeTabuada(){
     const numero = document.getElementById('tabuada').value;
     let divResult = document.getElementById('result')
