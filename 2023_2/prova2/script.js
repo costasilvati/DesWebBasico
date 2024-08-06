8f3e8655812e9ea453285dd15eb42706b4575a53
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2024-08-05 21:52:01 -0300

    add: aula 0508

---
+++ 2023_2/prova2/script.js
@@ -0,0 +1,28 @@
+	
+function.displayRadioValue(){ ariavaluemax
+    var elementos=document.getElementById('game');
+    for (i=0; i<elementos.clientHeight; i++){
+        if(elementos [i] checked){
+            document.getElementById(result).innerHTML
+            o jogados selecionado e +elements [i].value
+        }
+    }
+}
+
+function getoption (){ariavaluemax
+    let selectelement=document.querySelector(select1)
+    let output=selectelement.value
+    document.querySelector(output.textontert=output)
+}
+
+function getcheckbox(){ariavaluemax
+let getcheckbox=document.querySelectorAll('input[type="checkbox"]')
+let text=''
+   for(i=0; i<getcheckboxer.length; i++){
+    text=`${text} ${getcheckboxer[i].value}`
+    document.getElementById.apply(pri).innerHTML=text
+   }
+}
+function getElementById(){
+    text= `erro`
+}
---
+++ 2023_2/sub/java.js
@@ -0,0 +1,16 @@
+function enviar() {
+    var nome = document.getElementById('nome');
+    var estilo = document.getElementById('estilo');
+    if (nome.value.lenght === 0) {
+        alert('É necessário digitar o nome')
+        if (estilo.value.lenght === 0) {
+            alert('É necessário digitar o estilo')
+
+        } else {
+
+        }
+
+    }
+
+
+}
