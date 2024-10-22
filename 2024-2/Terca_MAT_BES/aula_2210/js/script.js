3fb939713d8cf0259cf6dadbb7faec621f819e25
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2024-10-22 11:27:29 -0300

    add: código aula 22/10/2024
    Javascript

---
+++ 2024-2/Terca_MAT_BES/aula_2210/js/script.js
@@ -0,0 +1,22 @@
+function mudaCor(){
+    document.getElementById('p1').style.color = "red";
+}
+
+function mudaTamanho(){
+    document.getElementById('p2').style.fontSize = '150%';
+}
+
+function mudaDisplay(){
+    var p3 = document.getElementById('p3');
+    if(p3.style.display === 'none'){
+        p3.style.display = 'block';
+    }else{
+        p3.style.display = 'none';
+    }
+    
+}
+
+
+function verMensagem(){
+    window.alert("Esta é uma mensagem");
+}
