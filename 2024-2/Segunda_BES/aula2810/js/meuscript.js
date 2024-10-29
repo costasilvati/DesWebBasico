a2d73068c1324848f89825703b16770263674f08
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2024-10-28 21:48:02 -0300

    add: codigo de aula 2810

---
+++ 2024-2/Segunda_BES/aula2810/js/meuscript.js
@@ -0,0 +1,7 @@
+function ligar(){
+    document.getElementById('lamp').src = "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+
+function desligar(){
+    document.getElementById('lamp').src = "https://www.w3schools.com/js/pic_bulboff.gif";
+}
