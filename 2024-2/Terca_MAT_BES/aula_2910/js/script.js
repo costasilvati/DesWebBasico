f53ac2169d337647a43dcadba980a3bbe9f21d16
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2024-11-05 11:23:06 -0300

    add: Código de aula 05/11

--- 2024-2/Terca_MAT_BES/aula_2910/js/script.js
+++ 2024-2/Terca_MAT_BES/aula_2910/js/script.js
@@ -11,5 +11,47 @@ function exibeDados(){
  }
 
  function verRadio(){
-    v
+    var f1 = document.getElementById('f1').checked;
+    if(f1){
+       alert("Que bom que você é feliz!"); 
+    }else if(document.getElementById('f2').checked){
+        alert("Que pena que você não é feliz");
+    }else if(document.getElementById('f3').checked){
+        alert("Você é feliz as vezes...");
+    }
  }
+// versão com laço de repetição
+ function verRadio2(){
+    var vetorRadio = document.getElementsByName('f'); // retorna um vetor de elementos
+    for(i=0; i< vetorRadio.length; i++){
+        if(vetorRadio[i].checked){
+            document.getElementById('feliz').innerHTML = 
+                                            "Feliz: "+ vetorRadio[i].value;
+        }
+    }
+}
+
+function verSelect(){
+    let element = document.querySelector('#curso');
+    let output = element.value;
+    document.querySelector('.output').textContent = output;
+}
+
+function verCheckBox(){
+    let checkBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    let text = "Estilos Musicais: ";
+    for(i=0;i<checkBoxes.length;i++){
+        text = `${text} ${checkBoxes[i].value},`;
+    }
+    document.getElementById('check').innerHTML = text;
+}
+
+/*
+Desnvolver uma função que exiba todos os dados do formulário da seguinte forma
+a açã deve ser executada com o clique de apenas um botão: Salvar
+Nome: xxxx
+Senha: xxxx
+Feliz: xxxxx
+Curso: xxxx
+Estilo: xxxxx
+*/
