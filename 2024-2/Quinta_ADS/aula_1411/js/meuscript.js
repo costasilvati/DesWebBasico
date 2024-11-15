3cbe3d148766a118e474a0ce6c0fc4793db7bd87
Juliana Costa-Silva <costasilvati@users.noreply.github.com>
2024-11-14 21:54:24 -0300

    add: código de aula

--- 2024-2/Quinta_ADS/aula_1411/js/meuscript.js
+++ 2024-2/Quinta_ADS/aula_1411/js/meuscript.js
@@ -1,6 +1,45 @@
-let i = 1;
+let i = 1; // variável global
 
-function salvar() {
+function salvar(){
+    let nome = document.querySelector('#n').value;
+    nome = nome + ' '+ document.getElementById('sn').value;
+    let email = document.getElementById('e').value;
+    // Pegar body da tabela
+    let tbody = document.querySelector('.table tbody');
+    let linha = document.createElement('tr');
+    // Cria os table data (td)
+    let colId = document.createElement('td');
+    let colNome = document.createElement('td');
+    let colEmail = document.createElement('td');
+    // Coloca os valore nos td's
+    i++;
+    colId.innerHTML = i;
+    colNome.innerHTML = nome;
+    colEmail.innerHTML = email; 
+    // Associa td's a linha
+    linha.appendChild(colId);
+    linha.appendChild(colNome);
+    linha.appendChild(colEmail);
+    // Coloca linha no corpo da tabela
+    tbody.appendChild(linha);
+}
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+function salvar2() {
     let nome = document.querySelector('#n').value;
     nome = nome + ' ' + document.querySelector('#sn').value;
     let email = document.getElementById('e').value;
